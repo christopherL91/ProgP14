dcf2e3d7df0227c7c09940203646cf86f6d167a2
Christopher Lillthors <lillt@kth.se>
2014-05-19 18:39:07 +0200

    Added color output, config reading

--- ATM/ServerFolder/server.go
+++ ATM/ServerFolder/server.go
@@ -1,31 +1,78 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2014 Christopher Lillthors
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package main
 
-//ATM server.
-
 import (
-	// "fmt"
 	"code.google.com/p/gcfg"
+	"flag"
+	"github.com/wsxiaoys/terminal/color"
 	"net"
+	"os"
+	"runtime"
 	"sync"
 	"time"
-	"runtime"
 )
 
+//			Config
+/*---------------------------------------------------*/
+//Configuration stuff.
+var (
+	configPath string
+	prompt     = "Unicorn@ATM>"
+	version    = 1.0
+	author     = "Christopher Lillthors. Unicorn INC"
+	width      = 80
+)
+
+//Struct to hold all the configurations.
+type Config struct {
+	Server struct {
+		Address string
+		Port    string
+	}
+}
+
+/*---------------------------------------------------*/
+
+//			Server/Client
+/*---------------------------------------------------*/
 //A convenience type.
 type menu []string
 
 type client struct {
-	conn    net.Conn
-	id int
-	
+	conn net.Conn
+	id   int
 }
 
 type server struct {
-	clients []client
+	clients map[*client]bool //For fast checkup if the client is connected.
 	mutex   *sync.Mutex
 	menus   map[string]menu
 }
 
+/*---------------------------------------------------*/
+
+//			Configure for your own good.
 /*---------------------------------------------------*/
 /*Everything here will be printed out to the client.*/
 var (
@@ -54,16 +101,11 @@ var (
 
 /*---------------------------------------------------*/
 
-const (
-	//Use this to check how long the banner is.
-	width = 80
-)
-
 func newServer() *server {
 	return &server{
-		clients: make([]client,1)
-		mutex: new(sync.Mutex),
-		menus: make(map[string]menu, 2),
+		clients: make(map[*client]bool),
+		mutex:   new(sync.Mutex),
+		menus:   make(map[string]menu),
 	}
 }
 
@@ -79,21 +121,35 @@ func (s *server) numOfMenus() int {
 	return len(s.menus)
 }
 
-func (s *server) addClient() {
-	
+func (s *server) addClient(c *client) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.clients[c] = true
 }
+
 func checkError(err error) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		color.Printf("@{r}Fatal error %s", err.Error())
 		os.Exit(1)
 	}
 }
 
 func init() {
+	//For configurations.
+	flag.StringVar(&configPath, "config", "server.gcfg", "Path to config file")
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
 func main() {
+	/*---------------------------------------------------*/
+	config := new(Config)
+	color.Println("\t\t\t\t@{b}ATM started")
+	color.Println("@{g}Reading config file...")
+	err := gcfg.ReadFileInto(config, configPath)
+	checkError(err)
+	color.Println("@{g}Config read OK")
+	/*---------------------------------------------------*/
+
 	/*---------------------------------------------------*/
 	/*Setup area. create a new server*/
 	server := newServer()
